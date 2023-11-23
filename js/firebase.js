acde32abc09bc2d71bfab0275567089de4a4e8c0
occlude <aaribeiro10@gmail.com>
2023-11-23 05:07:57 -0300

    Firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,14 @@
 (function () {
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyDDE5qV-rNsJ0P3ISK3GWkVp391-6-QfBk",
-        authDomain: "scoreenem-fc2b1.firebaseapp.com",
-        projectId: "scoreenem-fc2b1",
-        storageBucket: "scoreenem-fc2b1.appspot.com",
-        messagingSenderId: "358727950530",
-        appId: "1:358727950530:web:06b3407444f352f33b5b5b",
-        measurementId: "G-V4S23G70MS"
-      };
+  const firebaseConfig = {
+    apiKey: "AIzaSyBAXcpJupzH0exgTEqTnhDFCbru1d1HkB0",
+    authDomain: "scoreenem.firebaseapp.com",
+    projectId: "scoreenem",
+    storageBucket: "scoreenem.appspot.com",
+    messagingSenderId: "486560487123",
+    appId: "1:486560487123:web:78df947a9e83f44bc2a626",
+    measurementId: "G-SEZDPZBZLW"
+  };
 
     const app = firebase.initializeApp(firebaseConfig);
     
